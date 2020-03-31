b5e1f6723152f8342c63442efe6c706035484d35
Valdas <valdas@e-servisas.lt>
2020-03-31 12:18:01 +0300

    Sukurtas projektas

---
+++ README.md
@@ -0,0 +1,3 @@
+### Demo projektas apie visines ir anonimines klases
+
+Cia bus parodyta kaip is aniniminiu klasiu gimsta lambda israiska :)
---
+++ src/lt/bit/java2/Ano.java
@@ -0,0 +1,8 @@
+package lt.bit.java2;
+
+public class Ano {
+
+    public static void main(String[] args) {
+        System.out.println("Yes");
+    }
+}
